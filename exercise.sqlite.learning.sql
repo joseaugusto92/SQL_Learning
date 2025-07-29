f793e569814a774947875dd0b2bf4e04dff20504
joseaugusto92 <gutto.olveira@gmail.com>
2025-07-28 22:30:48 -0300

    Mais alguns exercicios de SQL feitos

--- exercise.sqlite.learning.sql
+++ exercise.sqlite.learning.sql
@@ -128,8 +128,107 @@ LIMIT 10;
 fazendo um teste para ver se comitar em git hub está dando certo. 26/07/2025
 
 
+2. Qual a quantidade de cidades únicas dos 
+vendedores do estado de Santa Catarina?
+
+SELECT COUNT(DISTINCT s.seller_city) 
+FROM sellers s 
+WHERE seller_state = 'SC'
 
 
+ 3. Qual a quantidade de cidades únicas de todos os 
+vendedores da base?
+
+SELECT COUNT(DISTINCT s.seller_city) 
+FROM sellers s
+
+4. Qual o número total de pedidos únicos acima de R$ 
+3.500
+
+SELECT COUNT( op.payment_value) 
+FROM order_payments op 
+WHERE op.payment_value > "3500"
+
+
+SELECT COUNT(oi.price) 
+FROM order_items oi  
+WHERE oi.price > "3500"
+
+
+ 5. Qual o valor médio do preço de todos os pedidos?
+
+
+SELECT AVG(price) 
+FROM order_items oi
+
+
+
+ 6. Qual o maior valor de preço entre todos os pedidos?
+ 
+ SELECT MAX(price) 
+ FROM order_items oi 
+
+
+7. Qual o menor valor de preço entre todos os pedidos?
+
+ SELECT MIN(price) 
+ FROM order_items oi 
+
+ 8. Qual a quantidade de produtos distintos vendidos 
+abaixo do preço de R$ 100.00?
+
+SELECT COUNT(DISTINCT product_id) 
+FROM order_items oi 
+WHERE price < "100"
+
+9. Qual a quantidade de vendedores distintos que 
+receberam algum pedido antes do dia 23 de setembro de 
+2016?
+
+SELECT COUNT(DISTINCT seller_id) 
+FROM order_items oi 
+WHERE shipping_limit_date < '2016-09-23'
+
+
+
+SELECT shipping_limit_date 
+FROM order_items oi 
+
+
+10. Quais os tipos de pagamentos existentes?
+
+SELECT DISTINCT payment_type 
+FROM order_payments op 
+
+ 11. Qual o maior número de parcelas realizado?
+ 
+ SELECT MAX(payment_installments) 
+ FROM order_payments op 
+ 
+ 12. Qual o menor número de parcelas realizado?
+ 
+ SELECT MIN(payment_installments) 
+ FROM order_payments op
+
+
+  13. Qual a média do valor pago EM cartão de crédito?
+  
+  SELECT AVG (payment_value)
+  FROM order_payments op 
+  WHERE payment_type = "credit_card"
+  
+  14. Quantos tipos de status para um pedido existem?
+  
+  SELECT COUNT (DISTINCT o.order_status) 
+  FROM orders o 
+  
+  
+   15. Quais os tipos de status para um pedido?
+   
+   SELECT DISTINCT o.order_status 
+   FROM orders o 
+  
+  
 
 
 
